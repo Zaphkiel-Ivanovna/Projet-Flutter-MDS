6d115a0b0bb2392b46b9a4e51b4453a1ed514b67
DUBERNET Damien <damien@helyx.io>
2023-04-06 16:47:54 +0200

    ♻️ Lignes : Add route_id

--- lib/models/lignes.dart
+++ lib/models/lignes.dart
@@ -1,12 +1,14 @@
 class Lignes {
+  final String route_id;
   final String route_long_name;
   final String route_short_name;
   final String route_color;
-  final List<List<double>> coordinates;
+  final List<List<List<double>>> coordinates;
   bool isFavorite;
 
   Lignes(
-      {required this.route_long_name,
+      {required this.route_id,
+      required this.route_long_name,
       required this.route_short_name,
       required this.route_color,
       required this.coordinates,
@@ -14,6 +16,7 @@ class Lignes {
 
   Map<String, dynamic> toJson() {
     return {
+      'route_id': route_id,
       'route_long_name': route_long_name,
       'route_short_name': route_short_name,
       'route_color': route_color,
@@ -26,15 +29,18 @@ class Lignes {
     isFavorite = !isFavorite;
   }
 
-  factory Lignes.fromJson(
-      String nameRoutes, String shortRoutes, String color, List coordinates) {
+  factory Lignes.fromJson(String routeId, String nameRoutes, String shortRoutes,
+      String color, List coordinates) {
     return Lignes(
+      route_id: routeId,
       route_long_name: nameRoutes,
       route_short_name: shortRoutes,
       route_color: color,
       coordinates: coordinates
-          .map((coordsList) =>
-              (coordsList as List).map((coords) => coords as double).toList())
+          .map((coordsList) => (coordsList as List)
+              .map((coords) =>
+                  (coords as List).map((coord) => coord as double).toList())
+              .toList())
           .toList(),
       isFavorite: false,
     );
@@ -42,18 +48,22 @@ class Lignes {
 
   factory Lignes.fromtest(Map<String, dynamic> json) {
     return Lignes(
+      route_id: json['route_id'],
       route_long_name: json['route_long_name'],
       route_short_name: json['route_short_name'],
       route_color: json['route_color'],
-      coordinates: (json['shape']['coordinates'][0] as List)
-          .map((coordsList) =>
-              (coordsList as List).map((coords) => coords as double).toList())
+      coordinates: (json['shape']['coordinates'] as List<dynamic>)
+          .map<List<List<double>>>((route) => (route as List)
+              .map<List<double>>((coordsList) => (coordsList as List)
+                  .map<double>((coords) => coords as double)
+                  .toList())
+              .toList())
           .toList(),
       isFavorite: json['isFavorite'] ?? false,
     );
   }
 
   String toText() {
-    return "$route_long_name, $route_short_name, $route_color, $coordinates";
+    return "$route_id, $route_long_name, $route_short_name, $route_color, $coordinates";
   }
 }
